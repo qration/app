d69037ddfc640f8ca3b1582063bf5043c40fe539
EleKtr1X <aarnav.thite@gmail.com>
2026-05-08 13:12:11 -0400

    feat: add searchbar to the top

--- src/lib/components/feed/Navbar.svelte
+++ src/lib/components/feed/Navbar.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
 	import wordmark from '$lib/assets/qration-wordmark.svg';
+	import TextInput from '$lib/components/ui/TextInput.svelte';
 </script>
 
 <div
-	class="flex flex-row items-center justify-between border-b-2 border-b-gray-300
-       bg-white p-4 text-4xl font-medium text-black">
-	<img src={wordmark} alt="Qration logo" class="h-10 cursor-pointer" />
-	<i class="ti ti-settings cursor-pointer"></i>
+	class="flex flex-row items-center gap-10 border-b-2 border-b-gray-300 bg-white
+		p-4 text-4xl font-medium text-black">
+	<div class="flex flex-1 justify-start">
+		<img src={wordmark} alt="Qration logo" class="h-10 cursor-pointer" />
+	</div>
+	<TextInput placeholder="Search..." iconClass="ti ti-search" cls="flex-6" />
+	<div class="flex flex-1 justify-end">
+		<i class="ti ti-settings cursor-pointer"></i>
+	</div>
 </div>
---
+++ src/lib/components/ui/TextInput.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	let {
+		placeholder,
+		iconClass = '',
+		clear = true,
+		cls = '',
+	}: {
+		placeholder: string;
+		iconClass?: string;
+		clear?: boolean;
+		cls?: string;
+	} = $props();
+
+	let input = $state('');
+	function clearText() {
+		input = '';
+	}
+</script>
+
+<div
+	class="{cls}flex w-full flex-row items-center rounded-xl border-2
+		border-gray-400 text-xl">
+	<i class="{iconClass}absolute pr-0 pl-2"></i>
+	<input
+		type="text"
+		{placeholder}
+		class="w-full rounded-xl border-none bg-transparent px-9 text-xl
+			placeholder:text-gray-400 active:border-none"
+		bind:value={input} />
+	{#if clear && input != ''}
+		<button
+			class="flex cursor-pointer items-center justify-end"
+			aria-label="Clear"
+			onclick={() => clearText()}>
+			<i class="ti ti-x absolute px-2 py-3"></i>
+		</button>
+	{/if}
+</div>

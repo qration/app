879852b2801e6cb140af1e347e358aff5a526e12
EleKtr1X <aarnav.thite@gmail.com>
2026-05-08 02:42:18 -0400

    fix: small ui tweaks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,11 @@
 
 <div class="flex h-screen flex-col">
 	<Navbar />
-	<div>
-		<div class="h-full flex-9">
-			<h1>Welcome to SvelteKit</h1>
-			<p>
-				Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
-				the documentation
-			</p>
-		</div>
+	<div class="p-4">
+		<h1>Welcome to SvelteKit</h1>
+		<p>
+			Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
+			the documentation
+		</p>
 	</div>
 </div>

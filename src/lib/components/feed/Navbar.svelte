e4be1bad7943e249703a33d099eafe7f7335ba27
EleKtr1X <aarnav.thite@gmail.com>
2026-05-08 02:30:47 -0400

    feat: add navbar + wordmark (i spent way too much time on this) + settings icon

---
+++ src/lib/components/feed/Navbar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import wordmark from '$lib/assets/qration-wordmark.svg';
+</script>
+
+<div
+	class="flex flex-row items-center justify-between border-b-2 border-b-gray-300
+       bg-white p-4 text-4xl font-medium text-black">
+	<img src={wordmark} alt="Qration logo" class="h-10 cursor-pointer" />
+	<i class="ti ti-settings cursor-pointer"></i>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-	documentation
-</p>
+<script>
+	import Navbar from '$lib/components/feed/Navbar.svelte';
+</script>
+
+<div class="flex h-screen flex-col">
+	<Navbar />
+	<div>
+		<div class="h-full flex-9">
+			<h1>Welcome to SvelteKit</h1>
+			<p>
+				Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
+				the documentation
+			</p>
+		</div>
+	</div>
+</div>

208384b32c4fdbe85bbccaf6887b073c5b55a109
EleKtr1X <aarnav.thite@gmail.com>
2026-05-09 01:05:47 -0400

    fix: make search bar rounded and aligned with the collapse button

--- src/lib/components/feed/ArticlesSidebar.svelte
+++ src/lib/components/feed/ArticlesSidebar.svelte
@@ -2,9 +2,9 @@
 	import TextInput from '$lib/components/ui/TextInput.svelte';
 </script>
 
-<div class="flex flex-col justify-start h-full border-gray-300 border-r-2">
+<div class="flex flex-col justify-start h-full border-gray-300 border-r-2 py-3 px-4">
 	<div
-		class="flex flex-row gap-10 border-b-2 border-gray-300 bg-white text-4xl text-black">
-		<TextInput placeholder="Search..." iconClass="ti ti-search " cls="flex-5 " />
+		class="flex flex-row gap-10 border-gray-300 bg-white text-4xl text-black">
+		<TextInput placeholder="Search..." iconClass="ti ti-search " />
 	</div>
 </div>
--- src/lib/components/ui/TextInput.svelte
+++ src/lib/components/ui/TextInput.svelte
@@ -18,12 +18,13 @@
 </script>
 
 <div
-	class="{cls}flex w-full flex-row items-center text-xl">
-	<i class="{iconClass}absolute pr-0 pl-2"></i>
+	class="{cls}flex w-full flex-row items-center rounded-xl border-2
+		border-gray-300 text-xl">
+	<i class="{iconClass}absolute pl-3"></i>
 	<input
 		type="text"
 		{placeholder}
-		class="w-full border-none bg-transparent px-9 text-xl
+		class="w-full rounded-xl border-none bg-transparent px-10 py-2 text-xl
 			placeholder:text-gray-400 active:border-none"
 		bind:value={input} />
 	{#if clear && input != ''}
@@ -31,7 +32,7 @@
 			class="flex cursor-pointer items-center justify-end"
 			aria-label="Clear"
 			onclick={() => clearText()}>
-			<i class="ti ti-x absolute px-2 py-3"></i>
+			<i class="ti ti-x absolute px-3 py-3"></i>
 		</button>
 	{/if}
 </div>

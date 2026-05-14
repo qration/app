dcae440eae39cfaf71781075470698723eca8cde
EleKtr1X <aarnav.thite@gmail.com>
2026-05-14 01:35:01 -0400

    fix: make theme swatches work on tab

--- src/lib/components/feed/Settings.svelte
+++ src/lib/components/feed/Settings.svelte
@@ -2,7 +2,7 @@
 	import { mode, setMode } from 'mode-watcher';
 	import Modal from '../ui/Modal.svelte';
 	import Button from '../ui/Button.svelte';
-	import Icon from '@iconify/svelte';
+	import Swatch from '../ui/Swatch.svelte';
 
 	let {
 		open = $bindable(false),
@@ -23,36 +23,18 @@
 		<label class="flex flex-col gap-2">
 			<span class="text-text-muted">Theme</span>
 			<div class="flex flex-row gap-4">
-				<label
-					class="border-border border-2 w-15 h-15 rounded bg-light-theme
-						cursor-pointer hover:border-text flex justify-center items-center"
-					class:border-text={mode.current == 'light'}>
-					{#if mode.current == 'light'}
-						<Icon icon="tabler:check" class="text-text text-3xl" />
-					{/if}
-					<input
-						type="radio"
-						name="theme"
-						id="light"
-						checked={mode.current == 'light'}
-						onclick={() => setMode('light')}
-						class="opacity-0 fixed w-0" />
-				</label>
-				<label
-					class="border-border border-2 w-15 h-15 rounded bg-dark-theme
-						cursor-pointer hover:border-text flex justify-center items-center"
-					class:border-text={mode.current == 'light'}>
-					{#if mode.current == 'dark'}
-						<Icon icon="tabler:check" class="text-text text-3xl" />
-					{/if}
-					<input
-						type="radio"
-						name="theme"
-						id="light"
-						checked={mode.current == 'dark'}
-						onclick={() => setMode('dark')}
-						class="opacity-0 fixed w-0" />
-				</label>
+				<Swatch
+					colour="light-theme"
+					name="theme"
+					id="light"
+					onclick={() => setMode('light')}
+					checked={mode.current == 'light'} />
+				<Swatch
+					colour="dark-theme"
+					name="theme"
+					id="dark"
+					onclick={() => setMode('dark')}
+					checked={mode.current == 'dark'} />
 			</div>
 		</label>
 		<div class="flex justify-end">
---
+++ src/lib/components/ui/Swatch.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	let {
+		checked,
+		onclick,
+		id,
+		name,
+		colour,
+	}: {
+		checked: boolean;
+		onclick: () => void;
+		id: string;
+		name: string;
+		colour: string;
+	} = $props();
+</script>
+
+<div>
+	<input type="radio" {name} {id} {checked} {onclick} class="peer sr-only" />
+	<label
+		for={id}
+		class="border-border border-2 w-15 h-15 rounded bg-{colour} cursor-pointer
+			hover:border-text flex justify-center items-center
+			peer-focus-visible:ring-2 peer-focus-visible:ring-blue-500"
+		class:border-text={checked}>
+		{#if checked}
+			<Icon icon="tabler:check" class="text-text text-3xl" />
+		{/if}
+	</label>
+</div>

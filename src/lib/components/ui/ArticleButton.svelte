ace5c50e8e97712a26718961892d0e3b1e574562
EleKtr1X <aarnav.thite@gmail.com>
2026-06-29 00:00:39 -0400

    feat: add summary to cards

--- src/lib/components/ui/ArticleButton.svelte
+++ src/lib/components/ui/ArticleButton.svelte
@@ -23,14 +23,21 @@
 
 <button
 	class="flex flex-col text-lg border-2 border-border items-start
-		text-text-muted hover:text-text
+		text-text-muted hover:text-text gap-1
 		{selected ? 'bg-bg-hover' : 'bg-bg-secondary'} hover:bg-bg-hover rounded
 		px-3 py-2 cursor-pointer min-w-0 max-w-full"
 	{onclick}>
-	<div class="font-medium truncate min-w-0 max-w-full">
+	<div
+		class="truncate text-left {article.read ? 'font-regular' : 'font-bold'}
+			min-w-0 max-w-full">
 		{article.read ? '' : '\u2022 '}{article.name}
 	</div>
-	<div class="flex flex-row justify-between w-full">
+	<div
+		class="line-clamp-2 min-w-0 max-w-full text-left text-text-secondary
+			text-base">
+		{article.content}
+	</div>
+	<div class="flex flex-row justify-between w-full text-base">
 		<div class="flex flex-row gap-2 items-center">
 			<Icon {icon} />
 			<span class="truncate min-w-0 max-w-full">

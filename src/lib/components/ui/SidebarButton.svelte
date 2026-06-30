cc9c45e70b8fae73cfbe9b36ef803bed2bcb35e3
EleKtr1X <aarnav.thite@gmail.com>
2026-06-29 21:02:18 -0400

    fix: weird auto-lint bug?

--- src/lib/components/ui/SidebarButton.svelte
+++ src/lib/components/ui/SidebarButton.svelte
@@ -34,7 +34,7 @@
 
 	function handleKeyDown(e: KeyboardEvent) {
 		if (e.target !== e.currentTarget) return;
-		if (e.key == ' ' || e.key == 'Enter') {
+		if (e.key === ' ' || e.key === 'Enter') {
 			e.preventDefault();
 			onclick?.();
 		}
@@ -44,39 +44,35 @@
 <div
 	role="button"
 	tabindex="0"
-	class="group p-1 w-full flex flex-row items-center cursor-pointer gap-2
-		text-text-muted {selected ? 'bg-bg-hover'
-		: 'bg-bg'} hovονεζδληπκβθνηroundπλζαα:hover:not(:has(button:hover)):not(:has(:popover-open))]:bg-bg-hover"
-		{onclick} onkeydown={handleKeyDown}>
-	<div class="flex justify-center items-center shrink-0 text-2xl leading-none">
+	class="group flex w-full cursor-pointer flex-row items-center gap-2 p-1
+    text-text-muted {selected ? 'bg-bg-hover' : 'bg-bg'} rounded hover:text-text
+    [&:hover:not(:has(button:hover)):not(:has(:popover-open))]:bg-bg-hover"
+	{onclick}
+	onkeydown={handleKeyDown}>
+	<div class="flex shrink-0 items-center justify-center text-2xl leading-none">
 		<Icon {icon} />
 	</div>
 	{#if !collapsed}
 		<div
-			class="whitespace-nowrap flex flex-row justify-between w-full"
+			class="flex w-full flex-row justify-between whitespace-nowrap"
 			transition:fade={{ duration: 500 }}>
 			{text}
 			{#if starrable}
 				<div class="m-0">
 					<div
-						class="opacity-0 group-hover:opacity-100
-							group-focus-within:opacity-100 group-has-[:popover-open]:opacity-100"
+						class="opacity-0 group-focus-within:opacity-100 group-hover:opacity-100
+              group-has-[:popover-open]:opacity-100"
 						class:opacity-100={selected}>
-						<!-- <IconButton
-							icon="tabler:star{starred ? '-filled' : ''}"
-							label="Star {text}"
-							class="-m-1 {selected ? 'bg-bg-hover!' : ''}
-								group-hover:bg-bg-hover"
-							onclick={handleOnStar} /> -->
 						<IconButton
 							icon="tabler:dots-vertical"
 							label="Details for {text}"
-							class="-m-1 {selected ? 'bg-bg-hover!' :
-								''}
-								gθμγξγεπδδοεεg-bg-hover" onclick={(e) => e.stopPropagation()}
-							popovertarget={menuId} />
-						<DetailsList {menuId} {starred} {onstar} />
+							popovertarget={menuId}
+							class="-m-1 {selected
+								? 'bg-bg-hover!'
+								: ''} group-hover:bg-bg-hover"
+							onclick={(e) => e.stopPropagation()} />
 					</div>
+					<DetailsList {menuId} {starred} {onstar} />
 				</div>
 			{/if}
 		</div>
